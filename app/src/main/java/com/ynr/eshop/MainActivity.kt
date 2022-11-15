59833a6708bd42ff15e986f170af041883acbc8e
Yashwant Nagar <nagaryash92@gmail.com>
2022-11-15 23:10:30 +0530

    Splash Screen Login Screen

--- app/src/main/java/com/ynr/eshop/MainActivity.kt
+++ app/src/main/java/com/ynr/eshop/MainActivity.kt
@@ -2,12 +2,24 @@ package com.ynr.eshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.ynr.eshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var activityMainBinding: ActivityMainBinding
+//    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
 }
