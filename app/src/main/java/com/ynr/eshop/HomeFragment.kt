ed4698f78392bebb10641ccacf46fcedca32f5e6
Yashwant Nagar <nagaryash92@gmail.com>
2022-11-16 23:41:17 +0530

    Product list screen

--- app/src/main/java/com/ynr/eshop/HomeFragment.kt
+++ app/src/main/java/com/ynr/eshop/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ynr.eshop.adapter.HomeAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,9 +20,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var itemRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +43,19 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        itemRecyclerView = view.findViewById(R.id.home_recycler_view)
+
+        itemRecyclerView.setHasFixedSize(true)
+        itemRecyclerView.layoutManager = GridLayoutManager(activity,2)
+
+        val homeAdapter = HomeAdapter()
+        itemRecyclerView.adapter = homeAdapter
+
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
